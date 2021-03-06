0f5f53f2f2d68288ffb5a748cd8e146a51269665
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-03-06 08:35:23 +0100

    creation de JS pour récuperer API

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
-main();
+    
+    const reponseJuste=false;
 
-    function main() {
-        const articles = getArticles(); // on recupere l'article grace a la fonction getArticle
-        afficheArticles(articles);// pour afficher les articles
-    };
+    (async function(){
+        const articles = await getArticles();
+        afficherArticles();
+    })
 
-    async function getArticles() { // la fonction getgetTousLesArticles pour recuperer de l'article directmement de l'API
-        fetch("http://localhost:3000/api/furniture")// utilisation de fetch pour chercher l'API avec l'url API
-            .then(function(httpBodyResponse) { // on attache la fonction qui va s'executer une fois qu'il a récuperer les données
-                return httpBodyResponse.json() //on traduit lebody en json
+    async function getArticles() {
+        fetch("http://localhost:3000/api/furniture")
+            .then(function(reponseBodyHttp) {
+                return reponseBodyHttp.json()
             })
-            .then(function(articles) {// on retourne lebody JSON dans le then suivant ici
-                console.log(articles)
-            });
+            .then(function(articles) {
+                console.log(articles);
+            })
     }
 
     async function afficheArticles() { // pour afficheer l'article 
         return ""; 
     }
+
+    
