ad40f4dd0f1bc01b9b34469df19e925b605e901b
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-03-15 17:00:38 +0100

    creation de fonction pour cliquer

--- js/produit.js
+++ js/produit.js
@@ -1,30 +1,47 @@
 (async () => {
     const meubleId = await getMeubleId() // Chercher l'identifation avec get depuis l'URL
-    const meubleData = await getMeubleData(meubleId) //request API avec l'argument meubleId pour ne chercher qu'une seule meuble dans l'API car il y a ID
-    afficherMeuble(meubleData)
+    const meubleData = await getMeubleData(meubleId)
+    displayMeuble(meubleData)
 })()
 
 function getMeubleId(){
-    //paramsURL
+    // L'extraction de l'ID pour idenitifier quel lien on a cliquer et plus tard pour afficher le bon API (qui se trouve dans ID)
+    
+    
+    //let params = (new URL(document.location)).searchParams// new pour créer une autre instance qu'on peut retourner plus tard
+    //let id = params.get('id')
+    
+    return (new URL(document.location)).searchParams.get('id')
+    //console.log(location)
 }
 
-function getMeubleData(meubleId){
-    return fetch(`http://localhost:3000/api/furniture/${meubleId}`)// meubleId est l'argument qui va apporteer l'id de l'API
-        .then(async (response) => {
-            response.json()
-            })
-        //.then(async (meubleData) => meubleData())
-        .catch((error) => {
-            alert(error) + `Error :(`
+function getMeubleData(meubleId) { // je crée une fetch pour chercher les données l'API et eventuellement l'API avec ID pour n'afficher qu'un seul donnée donc celui qui à le bon ID
+    return fetch(`http://localhost:3000/api/furniture/${meubleId}`)// j'imagine que la variable meubleId va injecter l'ID dans l'url donc chercher la bonne Id de l'API
+        .then(function(responseHttp) {
+            return responseHttp.json()
+        })
+        .catch(function(Error) {
+            alert(Error) + document.getElementById(`Error :(`)
         })
 }
 
-function afficherMeuble(meubleData) {
-    document.getElementById("blog__image").src = meubleData.imageUrl
-    document.getElementById("blog__title").textContent = meubleData.name
-    document.getElementById("blog__description").textContent = meubleData
-    document.getElementById("blog__price").textContent = meubleData //Si le prix est égal à 1 alors le prix reste si le prix est supérieur à 1 alors le prix est multiplier par le nombre si non error
-    document.getElementById("blog__choix").textContent = meubleData
-    document.getElementById("blog__choix").textContent = meubleData
+function displayMeuble(meubleData) { //j'imagine, Je vais afficher la bonne donnée que l'utilisateur à cliqué, directement modifier le DOM mais pas clonner comme da,ns la page index car il y en a qu'une seule donnée à afficher
+    document.getElementById('blog__image').src = meubleData.imageUrl
+    document.getElementById('blog__title').textContent = meubleData.name
+    document.getElementById('blog__description').textContent = meubleData.description
+    document.getElementById('blog__price').textContent = meubleData.price / 100 + " €"
+    document.getElementById('blog__option0').textContent = meubleData.varnish[0]
+    document.getElementById('blog__option1').textContent = meubleData.varnish[1]
+    document.getElementById('blog__option2').textContent = meubleData.varnish[2]
 
- } 
+    document.getElementById('buttonAdd').addEventListener('click', function(event) { // Je récupère les données saisis par l'utilmisateur en cliquant su r le bouton ajouter au panier
+        addArticleToFavorite(event, meubleData)
+        preventDefault()
+        event.stopPropagation()
+    })
+}
+
+
+function addArticleToFavorite(event, meubleData) {
+    
+}
