30ed347b6e057f686dfd5b1ac6fd3e669bccbacd
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-03-15 18:04:49 +0100

    latest update creation pour ajouter au pannier

--- js/produit.js
+++ js/produit.js
@@ -7,7 +7,6 @@
 function getMeubleId(){
     // L'extraction de l'ID pour idenitifier quel lien on a cliquer et plus tard pour afficher le bon API (qui se trouve dans ID)
     
-    
     //let params = (new URL(document.location)).searchParams// new pour créer une autre instance qu'on peut retourner plus tard
     //let id = params.get('id')
     
@@ -17,12 +16,10 @@ function getMeubleId(){
 
 function getMeubleData(meubleId) { // je crée une fetch pour chercher les données l'API et eventuellement l'API avec ID pour n'afficher qu'un seul donnée donc celui qui à le bon ID
     return fetch(`http://localhost:3000/api/furniture/${meubleId}`)// j'imagine que la variable meubleId va injecter l'ID dans l'url donc chercher la bonne Id de l'API
-        .then(function(responseHttp) {
-            return responseHttp.json()
-        })
-        .catch(function(Error) {
-            alert(Error) + document.getElementById(`Error :(`)
-        })
+        .then((responseHttp) => responseHttp.json())
+        .catch((Error) => {
+                alert(Error) + document.getElementById(`Error :(`)
+            })
 }
 
 function displayMeuble(meubleData) { //j'imagine, Je vais afficher la bonne donnée que l'utilisateur à cliqué, directement modifier le DOM mais pas clonner comme da,ns la page index car il y en a qu'une seule donnée à afficher
@@ -34,14 +31,16 @@ function displayMeuble(meubleData) { //j'imagine, Je vais afficher la bonne donn
     document.getElementById('blog__option1').textContent = meubleData.varnish[1]
     document.getElementById('blog__option2').textContent = meubleData.varnish[2]
 
-    document.getElementById('buttonAdd').addEventListener('click', function(event) { // Je récupère les données saisis par l'utilmisateur en cliquant su r le bouton ajouter au panier
-        addArticleToFavorite(event, meubleData)
-        preventDefault()
+    document.getElementById('buttonAdd').addEventListener('click', (event) => {
+        addArticleToCharts(event, meubleData)
+        event.preventDefault()
         event.stopPropagation()
     })
 }
 
-
-function addArticleToFavorite(event, meubleData) {
+function addArticleToCharts(event, meubleData) { // Si le produit est cliqué et supérieur ou egal à un on peut ajouter le produit au pannier si non error
+    const meubleAAjouter = getMeubleAAjouter() // meuble a ajouter va récuperer l'id de meuble pour ajouter au pannier
+    const meubleDejaAjouter = getMeubleDejaAjouter(meubleAAjouter, meubleData.id) // si le meuble est déjà au pannier, on peut toujours ajouter le panier tant que le produit est disponible si non message erreur "produit n'est plus disponible"
+    
     
 }
