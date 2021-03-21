884737192cfb9e9941b643830eda8100184edf80
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-03-21 13:33:04 +0100

    last input value modification

--- js/products.js
+++ js/products.js
@@ -2,7 +2,13 @@
     const meubleId = getMeubleId() // Chercher l'identifation avec get depuis l'URL
     const meubleData = await getMeubleData(meubleId)
     displayMeuble(meubleData)
-    buttonAndInput(meubleData)
+    //buttonAndInput(meubleData)
+
+    if(document.readyState == 'loading') { //Une fois que la page se télécharge le bouton va être pret aant les autres car si les boutons fonctionne après les autres, cela peut apporter des prpblèmes aux utilisateurs
+        document.addEventListener('DOMContentLoaded', ready)
+    }else{
+        ready(meubleData)
+    }
 })()
 
 function getMeubleId() {
@@ -27,78 +33,100 @@ function displayMeuble(meubleData) { //j'imagine, Je vais afficher la bonne donn
     document.getElementById('blog__image').src = meubleData.imageUrl
     document.getElementById('blog__title').textContent = meubleData.name
     document.getElementById('blog__description').textContent = meubleData.description
-    const price = document.getElementById('blog__price').textContent = meubleData.price / 100 + " €"
+    document.getElementById('blog__price').textContent = meubleData.price / 100 + " €"
     document.getElementById('blog__option0').textContent = meubleData.varnish[0]
     document.getElementById('blog__option1').textContent = meubleData.varnish[1]
     document.getElementById('blog__option2').textContent = meubleData.varnish[2]
-
-    // Evenement pour ajouter le produit au panier au moment de clique "ajouter au pannier"
-    //document.getElementById('buttonAdd').onclick('click', async (event) => {
 }
 
-function buttonAndInput(meubleData) {
+function ready(meubleData) {
+    // Evenement pour ajouter le produit au panier au moment de clique "ajouter au pannier"
+    //document.getElementById('buttonAdd').onclick('click', async (event) => {
+
     const buttonAjout = document.getElementById('buttonAdd')
-    buttonAjout.addEventListener('click', (event) => {
+    buttonAjout.addEventListener('click', async (event) => {
         //addArticleToCharts(event, meubleData)
         //const productsMeuble = await getMeubleProduits()
         //event.stopPropagation()
         event.preventDefault()
-        const ajoutMeuble = getAjoutMeuble(meubleData)
-        //getUpadePrice()
+        let ajoutMeuble = await getAjoutMeuble(meubleData, event)
+
+        //getUpdatePrice()
         // Une fonction pour aller à la page shopping avec le ID et le nom
+
+        quantityChanged(event)// Pour que si on choisit l'input value, l'input value et le prix va changer dans la page order
     })
 
     //ici pour input value pour dire aux utilisateurs que l'utilisateur ne peut choisir au moins 1 produit et non negative ou autre choses que le nombre
-    let quantityInput = document.getElementById("cart-quantity-input")
+    let quantityInput = document.getElementsByClassName("cart-quantity-input")
     for (let i = 0; i < quantityInput.length; i++) {
         let input = quantityInput[i];
+        input.addEventListener("change", (event) => {
+            quantityChanged(event)
+        })
     }
-    input.addEventListener("change", function(event) {
-        changeQuantite()
-    })
 }
 
-function getAjoutMeuble(meubleData) {
-    //L'user a maintenant un panier
-    let userPanier = JSON.parse(localStorage.getItem("userPanier") || [])
+function getAjoutMeuble(meubleData, event) {
 
-    //Récupération du panier dans le localStorage et ajout du produit dans le panier avant revoit dans le localStorage
-    userPanier.push(meubleData._id);
-    localStorage.setItem("userPanier", JSON.stringify(userPanier))
-    console.log("Administration : le produit a été ajouté au panier")
-    console.log(userPanier)
+    /*L'utilisateur à besoin d'un panier dans le localStorage de son navigateur
+Vérifier si le panier existe dans le localStorage, sinon le créer et l'envoyer dans le localStorage au premier chargement du site quelque soit la page*/
+
+    buttonAjout = event.target
+
+    if(localStorage.getItem("userPanier")){
+        console.log("Administration : le panier de l'utilisateur existe dans le localStorage");
+    }else{
+        console.log("Administration : Le panier n'existe pas, il va être créer et l'envoyer dans le localStorage");
+          //Le panier est un tableau de produits
+          let panierInit = [];
+          localStorage.setItem("userPanier", JSON.stringify(panierInit));
+      };
+    
+          //Tableau et objet demandé par l'API pour la commande
+          let contact;
+          let products = [];
+    
+        //L'user a maintenant un panier
+        let userPanier = JSON.parse(localStorage.getItem("userPanier"));
+
+        userPanier.push(meubleData._id);
+        localStorage.setItem("userPanier", JSON.stringify(userPanier))
+        console.log("Administration : le produit a été ajouté au panier")
+        console.log(userPanier)    /*L'user a maintenant un panier*/
 
     // dire si il y a au moins un meuble alors on incremente d'autres meubles si non on ajoute un meuble dans le panier
-
+      /*
     for (let i = 0; i < userPanier.length; i++) { // si on touche le bouton ajouter au panier plus de 1 fois alors on multiplie le prix avec le nombre lui même si non on ne multiplie qu'avec 1
         let meublePanier = userPanier[i];
         const price = document.getElementById('blog__price').innerText = meubleData.price / 100 + " €"
-        if (userPanier >= 1) {
+        if (userPanier.length >= 1) {
             meublePanier.incremente++,
                 total = meublePanier * price
-        } else {
+        } else if (userPanier.length === 1) {
             total = price * 1
         }
-    }
+    }*/
 }
 
+function quantityChanged(event) { //Lier l'input value au bouton pour dire si on choisi l'input plus de un alors si on clique le bouton ajouter le panier, il va avoir 2 produits qui va se mettre au localStorage et le prix se mutiplie en rapport avec le nombre choisi sur l'input value
+    let input = event.target
 
-function getUpadePrice() { //Lier l'input value au bouton pour dire si on choisi l'input plus de un alors si on clique le bouton ajouter le panier, il va avoir 2 produits qui va se mettre au localStorage et le prix se mutiplie en rapport avec le nombre choisi sur l'input value
-
-}
-
-/* Pour la page order, le bouton enlever, si on clique le bnouton enlever le prix descends
-function getMeublePricing(ajoutMeuble) { //la function qui permet de lier select option au prix, c'est a dire si je choisi deux le prix va doubler etc. Si je clique 2 fois, la page panier va doubler de sa quantité
-    //let cartContainer = document.getElementsByClassName("cart-container")[0]
-    let cartContainer = document.getElementById("cart-container")[0]
-    let cartRows = cartContainer.getElementById("cart-rows")
-    let totalPrice = 0
-    for (let i = 0; i < cartRows.length; i++) {
-        let cartRow = cartRows[i]
-        let priceElement = cartRow.getElementById("blog__price")
-        let quantityElement = cartRow.getElementById("cart-quantity-input")[0]
+    /*let containerAchat = document.getElementById("container-achat")
+    let rowAchat = containerAchat.getElementById("row-achat")*/
+    let colAchat = document.getElementById("col-achat")
+    for (let i = 0; i < colAchat.length; i++) {
+        const quantityAchat = colAchat[i];
+        let quantityElement = quantityAchat.getElementsByClassName("cart-quantity-input")[0]
         let quantity = quantityElement.value
-        totalPrice = (priceElement * quantity)    
+        if(isNaN(quantity) || quantity <= 0 || quantity > 100) { // Si le nombre n'est pas un chiffre ou le nombre est inférieur à 0 alors l'utilisateur n'as pas le droit de choisir si non il faut minimum 1 meuble
+            quantity = 1
+        }
     }
-    document.getElementById("blog__price").innerText = totalPrice
-}*/
+
+    console.log(input)
+    console.log(quantity)
+    console.log(quantityElement)
+    console.log(quantityAchat)
+    //getUpdatePrice()// Si on choisit la quantité de l'input le prix va changer en arrière plan et si on clique "ajouter au panier, le prix et la quantité va changer dans la page order"
+}
