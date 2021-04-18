bb37a3ab332cb98303bc155c9d2611c09165f491
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-04-18 22:37:39 +0200

    error confirmation update

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,7 +1,39 @@
 (() => {
     let confirmDatas = getConfirmData()
     displayData(confirmDatas)
+
+    getData()
+    if (document.readyState == 'loading') { //Une fois que la page se télécharge le bouton va être pret aant les autres car si les boutons fonctionne après les autres, cela peut apporter des prpblèmes aux utilisateurs
+        document.addEventListener('DOMContentLoaded', ready)
+    } else {
+        ready()
+    }
 })()
+//Recuperation du localStorage
+function getData(){
+        // Je réucpere les données de localStorage
+        let panierGetStorageData = localStorage.getItem('userPanier')
+
+        console.log(panierGetStorageData)
+    
+        // Verification qu'il ne soit pas vide
+        if (!panierGetStorageData) {
+            console.log("Oups, le panier est vide")
+        }
+    
+        let parseStructMeubleJSON = JSON.parse(panierGetStorageData)
+        return parseStructMeubleJSON    
+}
+
+// au moment de clique pour revenir à l'index, on efface le localStorage du panier
+function ready(){
+    document.addEventListener("DOMContentLoaded", () => {
+        let btn = document.getElementById("return__btn")
+        btn.addEventListener("click", () => {
+            window.localStorage.removeItem('userPanier');
+        })
+    })
+}
 
 function getConfirmData(){
     let recevoirSessionStorage = sessionStorage.getItem("order")//sessionStorage
@@ -14,6 +46,7 @@ function getConfirmData(){
         parsedSessionStorage
         console.log(parsedSessionStorage)
         sessionStorage.removeItem('order')// ici pour dire une fois qu'on quitte la page confirmation, le sessionStorage disparasse
+        //alert(error)
     }
 
     return parsedSessionStorage
